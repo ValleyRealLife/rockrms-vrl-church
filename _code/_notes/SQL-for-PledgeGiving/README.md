cef317f5827f7bf607f58552639b010ebd89bd18
Tim <47157075+doongmiin@users.noreply.github.com>
2025-03-18 08:48:58 -0700

    Init Commit

---
+++ _code/_notes/SQL-for-PledgeGiving/README.md
@@ -0,0 +1,8 @@
+# SQL for Pledge Giving
+i haven't quite wrapped my mind around this yet, but i know just enough to do what i need to do and move on.
+
+i'm writing this down in order to be able to come back to this.
+
+1. [This first SQL query](./EveryPersonThatGave.sql) shows a row for each Person that gave,
+2. [This second SQL query](./SpousesOfPersonsThatGave.sql) shows a row for each Person that is a spouse of a Person in the first SQL query,
+3. [This third SQL query]() is using the `UNION` operator in order to show in one result set the results from the above two SQL queries (i removed all columns except the [Id] because i needed to use this SQL in a DataView).
---
+++ _code/_notes/SQL-for-PledgeGiving/Union-PersonsAndSpouses.sql
@@ -0,0 +1,210 @@
+/**************************************************************************************************************
+    
+    DECLARE some variables up top.
+    
+**************************************************************************************************************/
+
+DECLARE @StartDate date = '12/1/2024'
+;
+DECLARE @EndDate date = '2/28/2025'
+;
+DECLARE @SoftCreditAttributeId int = (SELECT TOP 1 [Id] FROM [Attribute] WHERE [EntityTypeId] = 85 AND [Key] LIKE '%SoftCredit%')
+;
+
+DECLARE @ExcludedPersonIds TABLE(
+    [Id] int NOT NULL
+);
+
+DECLARE @CurrentPledges TABLE (
+    [GivingLeaderId] int
+  , [PledgeTotal] decimal (12,2)
+  , [AccountId] int
+  , [StartDate] date
+  , [EndDate] date
+  , [PledgeTypeValueId] int
+  , [AgeClassification] tinyint
+);
+
+DECLARE @SoftCreditGiving TABLE (
+    [GivingLeaderId] int
+  , [AccountId] int
+  , [GivingTotal] decimal(12,2)
+);
+
+DECLARE @PledgeGiving TABLE (
+    [GivingLeaderId] int
+  , [AccountId] int
+  , [GivingTotal] decimal(12,2)
+);
+
+/**************************************************************************************************************
+    
+    Populate the TABLE variables.
+    
+**************************************************************************************************************/
+
+INSERT INTO @ExcludedPersonIds ([Id])
+SELECT
+    p.[Id]
+FROM
+    [TaggedItem] ti
+    INNER JOIN [Person] p ON p.[Guid] = ti.[EntityGuid]
+WHERE
+    ti.[TagId] IN (14, 16, 17)
+    --TagId:14 is 'REST Key'
+    --TagId:16 is 'Rock Partner'
+    --TagId:17 is 'Test'
+;
+
+INSERT INTO @CurrentPledges (
+    [GivingLeaderId]
+  , [PledgeTotal]
+  , [AccountId]
+  , [StartDate]
+  , [EndDate]
+  , [PledgeTypeValueId]
+)
+SELECT
+    CASE WHEN pp.[AgeClassification] = 2 THEN pp.[Id] ELSE pp.[GivingLeaderId] END AS 'GivingLeaderId'
+    , SUM(fp.[TotalAmount]) AS 'PledgeTotal'
+    , fp.[AccountId]
+    , MIN(fp.[StartDate]) AS 'StartDate'
+    , MAX(fp.[EndDate]) AS 'EndDate'
+    , cdv.[Id] AS 'PledgeTypeValueId'
+FROM
+    [Person] pp
+    INNER JOIN [PersonAlias] pa ON pa.[PersonId] = pp.[Id]
+    INNER JOIN [FinancialPledge] fp ON fp.[PersonAliasId] = pa.[Id]
+    INNER JOIN [AttributeValue] av ON av.[EntityId] = fp.[Id] AND av.[AttributeId] = 16804
+    INNER JOIN [DefinedValue] cdv ON cdv.[Guid] = CAST(av.[Value] AS uniqueidentifier)
+WHERE 
+    av.[Value] IS NOT NULL        
+    AND cdv.[Id] IN (2004, 2005)
+    AND fp.[StartDate] >= '11/1/2024'
+    AND fp.[AccountId] IN (37,38)
+GROUP BY
+    CASE WHEN pp.[AgeClassification] = 2 THEN pp.[Id] ELSE pp.[GivingLeaderId] END
+  , fp.[AccountId]
+  , cdv.[Id]
+;
+
+
+INSERT INTO @SoftCreditGiving (
+    [GivingLeaderId]
+  , [AccountId]
+  , [GivingTotal]
+)
+SELECT
+    scp.[GivingLeaderId]
+  , ftd.[AccountId]
+  , SUM(ftd.[Amount])
+FROM
+    [FinancialTransactionDetail] ftd
+    INNER JOIN [FinancialTransaction] ft ON ft.[Id] = ftd.[TransactionId]
+    INNER JOIN [AttributeValue] scav ON scav.[EntityId] = ftd.[Id] AND scav.[AttributeId] = @SoftCreditAttributeId
+    INNER JOIN [Person] scp ON scp.[Id] = scav.[ValueAsPersonId]
+    LEFT JOIN @CurrentPledges cp ON cp.[GivingLeaderId] = scp.[GivingLeaderId] 
+        AND cp.[AccountId] = ftd.[AccountId] 
+        AND ft.[TransactionDateTime] >= cp.[StartDate]
+        AND ft.[TransactionDateTime] <= cp.[EndDate]
+WHERE
+    ftd.[AccountId] IN (37,38)
+    AND ft.[TransactionDateTime] BETWEEN @StartDate and @EndDate
+GROUP BY
+    scp.[GivingLeaderId]
+  , ftd.[AccountId]
+;
+
+
+INSERT INTO @PledgeGiving (
+    [GivingLeaderId]
+  , [AccountId]
+  , [GivingTotal]
+)
+SELECT
+    CASE WHEN p.[AgeClassification] = 2 THEN p.[Id] ELSE p.[GivingLeaderId] END
+  , ftd.[AccountId]
+  , SUM(ftd.[Amount])
+FROM
+    [FinancialTransactionDetail] ftd
+    INNER JOIN [FinancialTransaction] ft ON ft.[Id] = ftd.[TransactionId]
+    INNER JOIN [PersonAlias] pa ON pa.[Id] = ft.[AuthorizedPersonAliasId]
+    INNER JOIN [Person] p ON p.[Id] = pa.[PersonId]
+WHERE
+    ftd.[AccountId] IN (37,38)
+    AND ft.[TransactionDateTime] BETWEEN @StartDate AND @EndDate
+GROUP BY
+    CASE WHEN p.[AgeClassification] = 2 THEN p.[Id] ELSE p.[GivingLeaderId] END
+  , ftd.[AccountId]
+;
+
+/**************************************************************************************************************
+    
+    Now we get to the point.
+    
+**************************************************************************************************************/
+
+-- First query of union.
+SELECT
+    p.[Id]
+--   , p.[RecordTypeValueId]
+--   , p.[RecordStatusValueId]
+--   , p.[FirstName]
+--   , p.[NickName]
+--   , p.[LastName]
+--   , p.[Age]
+--   , p.[MaritalStatusValueId]
+--   , p.[IsEmailActive]
+--   , p.[PrimaryFamilyId]
+--   , p.[GivingLeaderId]
+FROM
+    [Person] p
+    LEFT JOIN [Person] spouse ON spouse.[PrimaryFamilyId] = p.[PrimaryFamilyId]
+        AND spouse.[Id] <> p.[Id]
+        AND spouse.[MaritalStatusValueId] = 143 --Married
+        AND p.[MaritalStatusValueId] = 143 --Married
+        AND spouse.[AgeClassification] <> 2
+WHERE
+    p.[Id] NOT IN (SELECT [Id] FROM @ExcludedPersonIds)
+    AND
+    p.[RecordTypeValueId] = 1
+    AND
+    (
+        p.[Id] IN (SELECT [GivingLeaderId] FROM @SoftCreditGiving)
+        OR
+        p.[Id] IN (SELECT [GivingLeaderId] FROM @PledgeGiving)
+    )
+
+UNION
+
+-- Second query of union
+SELECT
+    p.[Id]
+--   , p.[RecordTypeValueId]
+--   , p.[RecordStatusValueId]
+--   , p.[FirstName]
+--   , p.[NickName]
+--   , p.[LastName]
+--   , p.[Age]
+--   , p.[MaritalStatusValueId]
+--   , p.[IsEmailActive]
+--   , p.[PrimaryFamilyId]
+--   , p.[GivingLeaderId]
+FROM
+    [Person] p
+    LEFT JOIN [Person] spouse ON spouse.[PrimaryFamilyId] = p.[PrimaryFamilyId]
+        AND spouse.[Id] <> p.[Id]
+        AND spouse.[MaritalStatusValueId] = 143 --Married
+        AND p.[MaritalStatusValueId] = 143 --Married
+        AND spouse.[AgeClassification] <> 2
+WHERE
+    spouse.[Id] NOT IN (SELECT [Id] FROM @ExcludedPersonIds)
+    AND
+    spouse.[RecordTypeValueId] = 1
+    AND
+    (
+        spouse.[Id] IN (SELECT [GivingLeaderId] FROM @SoftCreditGiving)
+        OR
+        spouse.[Id] IN (SELECT [GivingLeaderId] FROM @PledgeGiving)
+    )
+;
